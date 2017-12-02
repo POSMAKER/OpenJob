84643bac0fce2bdca4f1fa992f373f0bb760ac29
raccoon <raccoon@DESKTOP-L7JIS4S>
2017-12-03 00:27:55 +0900

    aaa

--- OpenJob/src/main/java/com/open/job/MemberController.java
+++ OpenJob/src/main/java/com/open/job/MemberController.java
@@ -105,15 +105,7 @@ public class MemberController {
 		memberServ.insertMember(member);
 		return "true";
 	}
-	
-	
-<<<<<<< HEAD
-	 
-	//멤버 로그인 
-=======
-	
-	//멤버 로그인 프락
->>>>>>> branch 'master' of https://github.com/POSMAKER/OpenJob.git
+
 		@RequestMapping(value = "/loginproc", method = RequestMethod.POST)
 		public String loginproc(Member member, Model model) {
 
